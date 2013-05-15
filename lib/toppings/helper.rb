ffe011421a6101710161e1ae35390f7d2d7a59e6
Falk Hoppe <falkh@aenima.fritz.box>
2013-05-15 17:48:21 +0200

    made helper import more dynamic

--- lib/toppings/helper.rb
+++ lib/toppings/helper.rb
@@ -1,1 +1,4 @@
-require_relative 'helper/path_helper'
+# require other setup generator files
+Dir.glob(File.join("**", "helper", "*_helper.rb")).each do |helper|
+  require helper.gsub('lib/', '')
+end
