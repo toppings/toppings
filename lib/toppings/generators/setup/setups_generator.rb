23685522c6ec0c7032ece5a4fbf76c0245159ac0
Falk Hoppe <falkh@Aenima.local>
2013-05-24 13:28:53 +0200

    fixed file name bug

--- lib/toppings/generators/setup/setups_generator.rb
+++ lib/toppings/generators/setup/setups_generator.rb
@@ -3,7 +3,7 @@ module Toppings
     module Setup
       class SetupsGenerator < GroupGenerator
 
-        SETUP_FILES = %w{responsive_grid vertical_grid }
+        SETUP_FILES = %w{responsive_grid vertical_rhythm }
 
         def create_setup_files
           SETUP_FILES.each { |file| group_template_file file }
