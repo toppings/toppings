e1e894f46f5e6c2207701d96290f817bcec7981a
Falk Hoppe <falkhoppe81@gmail.com>
2013-05-26 12:20:38 +0200

    refactored generator call chain

---
+++ lib/toppings/helper/generator_registration_helper.rb
@@ -0,0 +1,35 @@
+module Toppings
+  module Helper
+    module GeneratorRegistrationHelper
+      extend ActiveSupport::Concern
+
+      def register_generator(*generators)
+        options = generators.last.is_a?(Hash) ? generators.pop : {}
+
+        raise "missing option :group for generator registration" unless options[:group]
+        group = options[:group]
+
+        generators.each do |generator|
+          group_generator(group, generator).start
+        end
+      end
+
+      private
+
+      def group_generator(group, generator)
+        generator = generator.to_s.camelcase
+
+        [generators_module_space,
+         group.camelcase,
+         "#{generator}Generator"].join('::').constantize
+      rescue
+        say "Generator #{generator} could not be found", :red
+      end
+
+      def generators_module_space
+        "Toppings::Generators"
+      end
+
+    end
+  end
+end
