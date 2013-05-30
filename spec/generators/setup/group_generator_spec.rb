b37051b31ff1de890483398b245d5c97cbbb7e33
Falk Hoppe <falkh@Aenima.local>
2013-05-30 17:49:03 +0200

    enabled tests for group generator

---
+++ spec/generators/setup/group_generator_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Toppings::Generators::Setup::GroupGenerator do
+
+  describe "sporting multiple stylesheet files" do
+    within_source_root do |tempdir|
+      tempdir = Pathname(tempdir)
+
+      FileUtils.mkdir_p tempdir.join(stylesheets_path)
+      FileUtils.touch tempdir.join(stylesheets_path).join("toppings.css.sass")
+    end
+
+    context "a group file" do
+      it "should be generated" do
+        subject.should generate(stylesheets_path.join("group/_base.css.sass"))
+      end
+
+      it "should be appended to the base file" do
+        subject.should generate(stylesheets_path.join('toppings.css.sass')) { |content|
+          content.should =~ /\@import \"group\/base\"/
+        }
+      end
+    end
+  end
+
+end
