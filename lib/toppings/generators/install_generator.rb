1780343b2ec61698c86f3489281afc413ea8b5bc
Falk Hoppe <falkh@Aenima.local>
2013-05-30 16:03:09 +0200

    minor refactorings and first generator specs

--- lib/toppings/generators/install_generator.rb
+++ lib/toppings/generators/install_generator.rb
@@ -7,6 +7,10 @@ module Toppings
       include Thor::Actions
       include Toppings::Helper::GeneratorRegistrationHelper
 
+      def notify_invoke
+        say "invoke InstallGenerator"
+      end
+
       def install_framework_structure
         register_generator :root_file, :fonts, :settings, :setups, :helper, group: 'setup'
       end
--- spec/generators/base_generator_spec.rb
+++ spec/generators/base_generator_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
-describe :base_generator do
-  it "it should provide an appropriate base name"
+describe Toppings::Generators::Setup::BaseGenerator do
+
+  context "as setup generator base class" do
+
+    context "with naming conventions" do
+
+      describe "the class name" do
+        it { subject[:described].stripped_class_name.should eq("BaseGenerator") }
+      end
+
+      describe "the classes base name" do
+        it { subject[:described].base_name.should eq("base") }
+      end
+
+    end
+
+    it "should output the invocation of the base generator" do
+      subject.should output("invoke Setup::BaseGenerator")
+    end
+
+  end
+
+
 end
