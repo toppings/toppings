e97129111409d44ce6ab6a0fb69de9c98c08815c
Falk Hoppe <falkh@Aenima.local>
2013-06-13 14:03:52 +0200

    fixed blobbed require statements for foreign folders, that include the toppings gem

--- lib/toppings/generators/setup.rb
+++ lib/toppings/generators/setup.rb
@@ -2,6 +2,6 @@ require 'toppings/generators/setup/base_generator'
 require 'toppings/generators/setup/group_generator'
 
 # require other setup generator files
-Dir.glob(File.join("**", "setup", "*_generator.rb")).each do |generator|
-  require generator.gsub('lib/', '')
+Dir.glob(File.join(Toppings.gem_root, "**", "setup", "*_generator.rb")).each do |generator|
+  require generator
 end
