ff26e702956e1e0d9791cde4751d528e0480a333
Falk Hoppe <falkh@Aenima.local>
2013-06-13 19:27:37 +0200

    enabling access to a default configuration file, placed in the gem itself.

--- lib/toppings/config.rb
+++ lib/toppings/config.rb
@@ -9,5 +9,21 @@ module Toppings
       end
     end
 
+    def default_config
+      @default_config ||= self.default_config = parse_default_config
+    end
+
+    def parse_default_config
+      config_file = File.read(default_config_path)
+      JSON.parse(config_file)
+    end
+
+    def default_config_path
+      Pathname.new(Toppings.gem_root).join('config', default_config_name)
+    end
+
+    def default_config_name
+      'default.json'
+    end
   end
 end
