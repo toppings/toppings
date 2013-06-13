827e46ca2e6964aa0d2dc9acd2cb66e1cebf4dce
Falk Hoppe <falkhoppe81@gmail.com>
2013-06-13 14:06:25 +0200

    added gem_root helper to toppings gem

--- lib/toppings.rb
+++ lib/toppings.rb
@@ -1,9 +1,16 @@
 require 'active_support/all'
 
+module Toppings
+  def self.gem_root
+    @gem_root ||= File.expand_path("../..", __FILE__)
+  end
+end
+
+
+require "toppings/helper"
 require "toppings/config"
 require "toppings/version"
-require "toppings/helper"
 
-module Toppings
+require 'pathname'
 
-end
+
