231f02f3c841da8895bc4ec1fbaa707be78df0dd
Falk Hoppe <falkhoppe81@gmail.com>
2013-06-16 15:55:36 +0200

    adjusted cli commands to rails generate style

--- lib/toppings/cli.rb
+++ lib/toppings/cli.rb
@@ -5,19 +5,27 @@ require "toppings/generators"
 
 module Toppings
   class CLI < Thor
+
     desc "install", "create a basic topping styles layout"
     def install
       Toppings::Generators::InstallGenerator.start
     end
 
-    desc "module", "create a new sass module"
-    def module(*args)
-      Toppings::Generators::ModuleGenerator.start(args)
+    desc "generate GENERATOR", "generating a toppings component like modules, settings etc."
+    def generate(generator = nil, *args)
+      case generator
+        when 'module'
+          Toppings::Generators::ModuleGenerator.start(args)
+        else
+          say "ERROR: generate was called with no generator argument"
+          say "USAGE: valid generators are font | module | setting | setup"
+      end
     end
 
-    desc "m", "shortcut for component"
-    def m(*args)
-      self.module(*args)
+    desc "g GENERATOR", "shortcut for generate"
+    def g(generator = nil, *args)
+      generate(generator, *args)
     end
+
   end
 end
