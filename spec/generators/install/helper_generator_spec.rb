3dd7da3fdd7c3cbfbb3126b65de6a6f2e7d8fa1b
Falk Hoppe <falkhoppe81@gmail.com>
2013-06-21 15:48:39 +0200

    added example for a sass check on template generation

---
+++ spec/generators/install/helper_generator_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'sass'
+
+describe Toppings::Generators::Install::HelperGenerator do
+  within_source_root do |tempdir|
+    tempdir = Pathname(tempdir)
+
+    FileUtils.mkdir_p tempdir.join(stylesheets_path)
+    FileUtils.touch tempdir.join(stylesheets_path).join("toppings.sass")
+  end
+
+  context "with naming conventions" do
+    it "should be generated" do
+      subject.should generate(stylesheets_path.join("helper/_debug.sass")) { |content|
+        sass_check(content).should be_true
+      }
+
+    end
+  end
+
+end
