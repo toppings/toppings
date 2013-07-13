fe7481d719c68bf7c3aec2707ac96094c5785687
Falk Hoppe <falkhoppe81@gmail.com>
2013-07-13 22:56:25 +0200

    registered susy as sass dependency

--- lib/toppings/generators/install/setups_generator.rb
+++ lib/toppings/generators/install/setups_generator.rb
@@ -2,7 +2,8 @@ module Toppings
   module Generators
     module Install
       class SetupsGenerator < GroupGenerator
-        with_templates :responsive_grid, :vertical_rhythm, :modular_scale
+        with_templates :vertical_rhythm, :modular_scale
+        with_templates :responsive_grid, dependencies: [:susy]
       end
     end
   end
