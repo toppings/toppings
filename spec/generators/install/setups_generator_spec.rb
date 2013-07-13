1557c8b5863162427a23c40c6bc6cf0bed313834
Falk Hoppe <falkhoppe81@gmail.com>
2013-07-13 18:26:29 +0200

    fixed content generation for standard sass mode …. some higher being may bless testing ^^

---
+++ spec/generators/install/setups_generator_spec.rb
@@ -0,0 +1,20 @@
+#require 'spec_helper'
+#
+#describe Toppings::Generators::Install::SetupsGenerator do
+#
+#  within_source_root do |tempdir|
+#    tempdir = Pathname(tempdir)
+#
+#    FileUtils.mkdir_p tempdir.join(stylesheets_path)
+#    FileUtils.touch tempdir.join(stylesheets_path).join("toppings.sass")
+#  end
+#
+#  context "providing some useful helper files" do
+#    describe "a debug helper" do
+#      it "should be created" do
+#        subject.should generate(stylesheets_path.join("helper/_debug.sass")) { |content| valid_sass?(content).should be_true }
+#      end
+#    end
+#  end
+#
+#end
