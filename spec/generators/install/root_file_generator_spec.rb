920cf08e997deb2862ca272dfc58414e8f3c6423
Falk Hoppe <falkhoppe81@gmail.com>
2013-07-16 18:10:31 +0200

    added check for vendor imports in root file

--- spec/generators/install/root_file_generator_spec.rb
+++ spec/generators/install/root_file_generator_spec.rb
@@ -15,4 +15,14 @@ describe Toppings::Generators::Install::RootFileGenerator do
     end
   end
 
+  context "as the dependency loader for all base dependencies" do
+    it "the root base file should include an import statement for each dependency" do
+      subject.should generate(stylesheets_path.join("toppings.sass")) { |content|
+        content.should include('@import "compass"')
+        content.should include('@import "compass/css3"')
+        content.should include('@import "compass/reset"')
+      }
+    end
+  end
+
 end
