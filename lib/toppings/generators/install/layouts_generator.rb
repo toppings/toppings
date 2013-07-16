acfbe5de02a102af97e37d1a8f7d7cea1867d001
Falk Hoppe <falkhoppe81@gmail.com>
2013-07-16 19:17:04 +0200

    added issue related todo

--- lib/toppings/generators/install/layouts_generator.rb
+++ lib/toppings/generators/install/layouts_generator.rb
@@ -2,7 +2,7 @@ module Toppings
   module Generators
     module Install
       class LayoutsGenerator < GroupGenerator
-
+        # TODO: think about adding some examples here (https://github.com/toppings/toppings/issues/43)
       end
     end
   end
