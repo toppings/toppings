1b81b6b31ae6828d57324e69ed7f3c400954b1d0
Falk Hoppe <falkhoppe81@gmail.com>
2013-11-20 18:20:34 +0100

    adjusted templates for components

--- lib/toppings/generators/structure/component_generator.rb
+++ lib/toppings/generators/structure/component_generator.rb
@@ -6,22 +6,26 @@ module Toppings
       class ComponentGenerator < Toppings::Generators::Structure::BaseGenerator
 
         def create_component_structure
-          component_template(type, name, "m-#{name}")
-          component_template(type, name, "l-#{name}")
+          component_template(type, name, "m-#{name}", template: 'module')
+          component_template(type, name, "l-#{name}", template: 'layout')
         end
 
         private
 
-        def component_template(type, component_name, target_name)
-          template rescued_sass_partial(type), component_path(component_name).join(sassy_file_name(target_name, partial: true))
+        def component_template(type, component_name, target_name, options = {})
+          template rescued_sass_partial(options[:template]), component_path(component_name).join(sassy_file_name(target_name, partial: true))
           append_import "#{component_name}/#{target_name}", structure_file_name(component_name)
         end
-
 
         def component_path(name)
           base_path.join(name)
         end
 
+        class << self
+          def source_root
+            template_path.join('structures', 'component')
+          end
+        end
       end
     end
   end
